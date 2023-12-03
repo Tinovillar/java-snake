f60219af543d933d18a5549ef802ebd01aa126ee
Tino <valennvillar@gmail.com>
2023-12-03 18:42:58 -0300

    gameloop created

---
+++ GameLoop.java
@@ -0,0 +1,19 @@
+public class GameLoop extends Thread{
+    private final Game game;
+    public GameLoop(Game game) {
+        this.game = game;
+        this.game.addFood();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!this.game.isPaused()) {
+                this.game.move();
+                sleep(150);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
